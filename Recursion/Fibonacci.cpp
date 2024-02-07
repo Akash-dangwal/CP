d35639285fb3d58613e91d21ee3683d9c93d398e
Akash-dangwal <154399788+Akash-dangwal@users.noreply.github.com>
2024-02-07 20:22:13 +0530

    Fibonacci

---
+++ Recursion/Fibonacci.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int Fibonacci(int n){
+	if(n==1||n==2) return 1;
+	return (Fibonacci(n-1)+Fibonacci(n-2));
+}
+int main(){
+	int n;
+	scanf("%d",&n);
+	int a=Fibonacci(n);
+	printf("%d",a);
+}
